c913b6db7a0b4c7d4dbb75b14872cf294f846297
Alfonso <alephmage@gmail.com>
2012-07-04 16:39:07 -0500

    test is now green

--- sayhello.rb
+++ sayhello.rb
@@ -1,3 +1,5 @@
 describe "RSpec speaks (title)" do
-  it "should say 'Hello from Rspec' when ran (bullet point)"
+  it "should say 'Hello from Rspec' when ran (bullet point)" do
+    print "Hello from RSpec"
+  end
 end
