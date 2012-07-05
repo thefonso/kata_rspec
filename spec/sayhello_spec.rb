d77e9c10f683f141d2988df981ba61cff6b4f16d
Alfonso <alephmage@gmail.com>
2012-07-05 15:43:15 -0500

    Done

--- spec/sayhello_spec.rb
+++ spec/sayhello_spec.rb
@@ -5,18 +5,11 @@ describe "sayhello.rb" do
   before :each do #this will create my object -before- -each- test is ran
     @hi = Hello.new
   end
-  
-    it "contains an object called Hello" do
-      @hi.should be_an_instance_of  Hello
-    end
     
-    describe "hello.speak" do 
-      it "should return Hello from RSpec" do
-        
-        $stdout.should_receive(:puts).with("Hello from RSpec")
-        
-        @hi.speak
-      end
-    end
-
+  it "should return Hello from RSpec" do
+    
+    $stdout.should_receive(:puts).with("Hello from RSpec")
+    
+    @hi.speak
+  end
 end
