0e2f4840991fd0bb6f3f7105e5c2efff58d74af5
Alfonso <alephmage@gmail.com>
2012-07-05 15:06:23 -0500

    moved a file around….nothing to see here

--- sayhello_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'sayhello.rb' #points to file so I can 'see' it
-
-describe "sayhello.rb (title)" do
-  it "should say 'Hello from Rspec' when ran (bullet point)" do
-    hi = Hello.new #brings my object into existence
-    hi.speak == "Hello from RSpec"
-  end
-end
