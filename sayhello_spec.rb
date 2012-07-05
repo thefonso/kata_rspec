b1cd2447ab83dd54128c82279d8c5426dd753386
Alfonso <alephmage@gmail.com>
2012-07-05 11:25:39 -0500

    separation of concerns...green

--- sayhello_spec.rb
+++ sayhello_spec.rb
@@ -1,9 +1,8 @@
-require_relative 'sayhello.rb' #just here to help
-
+require_relative 'sayhello.rb' #points to file so I can 'see' it
 
 describe "sayhello.rb (title)" do
   it "should say 'Hello from Rspec' when ran (bullet point)" do
-    hi = Hello.new
-    hi.speak.should == "Hello from RSpec"
+    hi = Hello.new #brings my object into existence
+    hi.speak == "Hello from RSpec"
   end
 end
